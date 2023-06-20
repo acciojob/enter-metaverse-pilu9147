2649b78e9548de08f67d82254bedf5b38a62afc7
pilu9147 <119696172+pilu9147@users.noreply.github.com>
2023-06-20 17:19:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let btn = document.getElementById("enterBtn")
+
+btn.addEventListener('click',()=>{
+	let p = document.getElementById("status");
+	p.innerHTML = "";
+	let h = document.getElementsByTagName("h1")[0];
+	h.innerHTML = "Entered Metaverse"
+})
